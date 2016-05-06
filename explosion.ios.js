657d0080295a04a4fd9ee3e157eb4ad8c7d33fca
Josh Sommer <JoshDSommer@gmail.com>
2016-05-06 10:05:45 -0400

    added explosion.ios.js

---
+++ explosion.ios.js
@@ -0,0 +1,14 @@
+ /***************************************************************************************
+ * Made for the {N} community by Brad Martin @BradWayneMartin
+ * https://twitter.com/BradWayneMartin
+ * https://github.com/bradmartin
+ * http://bradmartin.net
+ * Open Source Lib : https://github.com/tyrantgit/ExplosionField
+ *************************************************************************************/
+
+var app = require("application");
+
+function explode (view) {
+        return;
+}
+exports.explode = explode;
